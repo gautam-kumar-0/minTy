e53169398dc1c1f2cf145fe03ddd13a694ba524a
Gautam Kumar <gk7258182@gmail.com>
2024-12-02 10:22:12 +0530

    switch to vs code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import Keyboard from "./components/Keyboard"
-import './App.css'
-import TextWindow from './components/TextWindow'
+import Keyboard from "./components/Keyboard";
+import "./App.css";
+import TextWindow from "./components/TextWindow";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const text = "Lorem ipsum consectetur adipisicing dolorum ad ratione nulla obcaecati explicabo eligendi. Possimus adipisci assumenda fugit harum iure qui neque obcaecati"
-  return (
-    <>
-    <TextWindow text={text}></TextWindow>
-      <Keyboard></Keyboard>
-    </>
-  )
+	// const [count, setCount] = useState(0)
+	const text =
+		"Lorem ipsum consectetur adipisicing dolorum ad ratione nulla obcaecati explicabo eligendi. Possimus adipisci assumenda fugit harum iure qui neque obcaecati";
+	return (
+		<>
+			<TextWindow text={text}></TextWindow>
+			<Keyboard></Keyboard>
+		</>
+	);
 }
 
-export default App
+export default App;
