96d31d8c022bd2249b67a4b04897334c65b3ed38
Gautam Kumar <gk7258182@gmail.com>
2024-12-02 11:17:52 +0530

    keyboard row

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,47 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from "react";
 
 // Key Component
-const Key = React.memo(({ keyName, isActive }) => {
-  
-  const style = {
-    backgroundColor: isActive ? 'rgba(250,200,200,0.5)' : 'transparent',
-    border: '1px solid black',
-    padding: '10px 15px',
-    margin: '5px',
-    display: 'inline-block',
-    borderRadius: '10px',
-    transition: 'background-color 100ms ease-in'
-  };
-  
-  return <div style={style}>{keyName}</div>;
-});
+
+const Key = ({keyName, isActive}) => {
+	const style = {
+		backgroundColor: isActive ? "rgba(250,200,200,0.5)" : "transparent",
+		border: "1px solid black",
+		padding: "10px 15px",
+		margin: "5px",
+		display: "inline-block",
+		borderRadius: "10px",
+		transition: "background-color 100ms ease-in",
+	};
+
+	return <div style={style}>{keyName}</div>;
+};
 
 // Parent Component
 const Keyboard = () => {
-  const [activeKey, setActiveKey] = useState('');
+	const [activeKey, setActiveKey] = useState("");
 
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      setActiveKey(e.key);
-      setTimeout(() => setActiveKey(''), 100); // Reset after 500ms for feedback
-    };
+	useEffect(() => {
+		const handleKeyDown = (e) => {
+			setActiveKey(e.key);
+			setTimeout(() => setActiveKey(""), 100); // Reset after 500ms for feedback
+		};
 
-    window.addEventListener('keydown', handleKeyDown);
+		window.addEventListener("keydown", handleKeyDown);
 
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+		};
+	}, []);
 
-  const keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+	const keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+	const row1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"];
+	const row2 = [];
 
-  return (
-    <div>
-      {keys.map((key) => (
-        <Key key={key} keyName={key} isActive={key === activeKey} />
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			{keys.map((key) => (
+				<Key key={key} keyName={key} isActive={key === activeKey} />
+			))}
+		</div>
+	);
 };
 
 export default Keyboard;
