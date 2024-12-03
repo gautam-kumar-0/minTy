282288e9ffb60d9d198ddfa4334b66d764670423
Gautam Kumar <gk7258182@gmail.com>
2024-12-03 11:21:43 +0530

    Keyword as intended

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import TextWindow from "./components/TextWindow";
 
 function App() {
 	// const [count, setCount] = useState(0)
-	const text =
-		"Lorem ipsum consectetur adipisicing dolorum ad ratione nulla obcaecati explicabo eligendi. Possimus adipisci assumenda fugit harum iure qui neque obcaecati";
 	return (
 		<>
-			<TextWindow text={text}></TextWindow>
 			<Keyboard></Keyboard>
 		</>
 	);
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,29 +1,61 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 // Key Component
+import "./Keyboard.css";
+import TextWindow from "./TextWindow";
+const Key = React.memo(({keyName, isActive}) => {
+	const keyRef = useRef(null);
+	const glow = "glow";
+	useEffect(() => {
+		if (keyRef.current && isActive) {
+			keyRef.current.classList.add(glow);
+			setTimeout(() => {
+				keyRef.current.classList.remove(glow);
+			}, 125);
+		}
+	}, [isActive]);
+	return (
+		<div ref={keyRef} className="key">
+			{keyName}
+		</div>
+	);
+});
 
-const Key = ({keyName, isActive}) => {
-	const style = {
-		backgroundColor: isActive ? "rgba(250,200,200,0.5)" : "transparent",
-		border: "1px solid black",
-		padding: "10px 15px",
-		margin: "5px",
-		display: "inline-block",
-		borderRadius: "10px",
-		transition: "background-color 100ms ease-in",
-	};
-
-	return <div style={style}>{keyName}</div>;
+const Row = ({keys, className, activeKey}) => {
+	return (
+		<div className={`${className} row`}>
+			{keys.map((key) => {
+				return <Key key={key} keyName={key} isActive={key == activeKey} />;
+			})}
+		</div>
+	);
 };
 
-// Parent Component
+const LastRow = ({className, activeKey}) => {
+	const space = " ";
+	return (
+		<div className={`${className} row`}>
+			<Key key={space} keyName={"space"} isActive={space == activeKey} />
+		</div>
+	);
+};
+
+// parent component
 const Keyboard = () => {
 	const [activeKey, setActiveKey] = useState("");
-
+	const [inputText, setInputText] = useState("");
+	const [isTyping, setIsTyping] = useState(true);
+	const handleInput = function (key) {
+		if (!isTyping) setIsTyping(true);
+		else inputText += key;
+		// todo update text window to show key has been typed
+	};
 	useEffect(() => {
 		const handleKeyDown = (e) => {
 			setActiveKey(e.key);
-			setTimeout(() => setActiveKey(""), 100); // Reset after 500ms for feedback
+			console.log(e.key, e);
+			setTimeout(() => setActiveKey(""), 50); // Reset after 500ms for feedback
+			handleInput(e.key);
 		};
 
 		window.addEventListener("keydown", handleKeyDown);
@@ -33,16 +65,25 @@ const Keyboard = () => {
 		};
 	}, []);
 
-	const keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 	const row1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"];
-	const row2 = [];
+	const row2 = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\"];
+	const row3 = ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
+	const row4 = [" "];
+	const keys = [row1, row2, row3, row4];
+	const text =
+		"Lorem ipsum consectetur adipisicing dolorum ad ratione nulla obcaecati explicabo eligendi. Possimus adipisci assumenda fugit harum iure qui neque obcaecati";
 
 	return (
-		<div>
-			{keys.map((key) => (
-				<Key key={key} keyName={key} isActive={key === activeKey} />
-			))}
-		</div>
+		<section className="main">
+			<TextWindow text={text}></TextWindow>
+
+			<div className="keyboard">
+				<Row keys={row1} className={`row-1`} activeKey={activeKey} />
+				<Row keys={row2} className={`row-2`} activeKey={activeKey} />
+				<Row keys={row3} className={`row-3`} activeKey={activeKey} />
+				<LastRow className={`row-4`} activeKey={activeKey} />
+			</div>
+		</section>
 	);
 };
 
