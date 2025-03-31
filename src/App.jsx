cdd4c45ea96428a3bab51002166814b9af285525
gautam_kumar <gk7258182@gmail.com>
2025-03-31 18:30:22 +0530

    Header fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,77 @@
-import React, {memo} from "react";
+import React, {memo, useEffect, useRef, useState} from "react";
 import {RiSettings4Fill} from "react-icons/ri";
-import "./App.css";
+import {PiKeyboardLight} from "react-icons/pi";
+import {FaHeart} from "react-icons/fa";
+import {Link, Route, Routes} from "react-router-dom";
 import {Toaster} from "react-hot-toast";
 
+import "./App.css";
 import Fadable from "./components/Fadable/Fadable";
-
-import {PiKeyboardLight} from "react-icons/pi";
-import {Link, Route, Routes} from "react-router-dom";
 import Main from "./pages/Main";
 import Setting from "./pages/Setting";
 import useAppearance from "./hooks/useAppearance";
 import useLocalSetting from "./hooks/useLocalSetting";
-import {FaGithub, FaHeart} from "react-icons/fa";
 
-const Header = memo(() => (
-	<Fadable className="header">
-		<header>
-			<div>
-				<Link to="/" className="logo">
-					<h1>minTY</h1>
-					<PiKeyboardLight />
-				</Link>
-				<nav>
-					<Link to="/setting">
-						<RiSettings4Fill />
-					</Link>
-				</nav>
-			</div>
-			<div className="project-info">
-				<a href="https://github.com/gautam-kumar-0/minTy">
-					&lt;View Source/&gt;
-				</a>
-				<span>
-					Made with <FaHeart />
-				</span>
-			</div>
-		</header>
-	</Fadable>
-));
+const Header = memo(() => {
+	const headerRef = useRef(null);
+	const [prevScroll, setPrevScroll] = useState(window.scrollY);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const currentScroll = window.scrollY;
+			if (currentScroll < prevScroll) {
+				headerRef.current.classList.add("show");
+			} else {
+				headerRef.current.classList.remove("show");
+			}
+			setPrevScroll(currentScroll);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, [prevScroll]);
+
+	return (
+		<Fadable>
+			<header className="header" ref={headerRef}>
+				<div>
+					<Logo />
+					<Navigation />
+				</div>
+				<ProjectInfo />
+			</header>
+		</Fadable>
+	);
+});
+
+const Logo = () => (
+	<Link to="/" className="logo">
+		<h1>minTY</h1>
+		<PiKeyboardLight />
+	</Link>
+);
+
+const Navigation = () => (
+	<nav>
+		<Link to="/setting">
+			<RiSettings4Fill />
+		</Link>
+	</nav>
+);
+
+const ProjectInfo = () => (
+	<div className="project-info">
+		<a href="https://github.com/gautam-kumar-0/minTy">&lt;View Source/&gt;</a>
+		<span>
+			Made with <FaHeart />
+		</span>
+	</div>
+);
 
 function App() {
 	useLocalSetting();
 	useAppearance();
+
 	return (
 		<div className="app">
 			<Header />
