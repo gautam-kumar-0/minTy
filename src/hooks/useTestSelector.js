7400c348dd85593a7f219325232860bb78b23365
gautam_kumar <gk7258182@gmail.com>
2025-04-02 18:25:44 +0530

    deploy

--- src/hooks/useTestSelector.js
+++
@@ -1,1 +0,0 @@
-// This file has been removed as it is no longer needed. Redux hooks (useSelector and useDispatch) are used directly.
--- src/pages/Setting.jsx
+++
@@ -1,8 +0,0 @@
-import SettingsPanel from "../features/Setting/SettingsPanel";
-const Setting = () => (
-	<main>
-		<SettingsPanel />
-	</main>
-);
-
-export default Setting;
